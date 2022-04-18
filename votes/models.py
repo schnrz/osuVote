bebf20ae051382d9a0b407a9ad3195644fa6cc36
Brayan <brayansachez67@hotmail.com>
2022-04-18 14:01:50 -0500

    new model

--- votes/models.py
+++ votes/models.py
@@ -91,94 +91,30 @@
 from djongo import models
 from django import forms
 
-class Votos_std(models.Model):
-    puntos = models.IntegerField()
-    id_player = models.IntegerField()
-    nickname = models.CharField(max_length=20)
+class Player(models.Model):
+    player_id = models.IntegerField()
+    username = models.CharField(max_length=15)
+    points = models.SmallIntegerField()
 
     class Meta:
         abstract = True
 
-class Votos_stdForm(forms.ModelForm):
+class PlayerForm(forms.ModelForm):
     class Meta:
-        model = Votos_std
+        model = Player
         fields = (
-            'puntos', 'id_player', 'nickname'
+            'player_id', 'username', 'points'
         )
 
-class Votos_taiko(models.Model):
-    puntos = models.IntegerField()
-    id_player = models.IntegerField()
-    nickname = models.CharField(max_length=20)
+class Vote(models.Model):
+    player_id = models.IntegerField()
+    username = models.CharField(max_length=15)
+    avatar_url = models.CharField(max_length=100)
+    cover_url = models.CharField(max_length=100)
 
-    class Meta:
-        abstract = True
-
-class Votos_taikoForm(forms.ModelForm):
-    class Meta:
-        model = Votos_taiko
-        fields = (
-            'puntos', 'id_player', 'nickname'
-        )
-
-
-class Votos_ctb(models.Model):
-    puntos = models.IntegerField()
-    id_player = models.IntegerField()
-    nickname = models.CharField(max_length=20)
-
-    class Meta:
-        abstract = True
-
-class Votos_ctbForm(forms.ModelForm):
-    class Meta:
-        model = Votos_ctb
-        fields = (
-            'puntos', 'id_player', 'nickname'
-        )
-
-
-class Votos_mania(models.Model):
-    puntos = models.IntegerField()
-    id_player = models.IntegerField()
-    nickname = models.CharField(max_length=20)
-
-    class Meta:
-        abstract = True
-
-class Votos_maniaForm(forms.ModelForm):
-    class Meta:
-        model = Votos_mania
-        fields = (
-            'puntos', 'id_player', 'nickname'
-        )
-
-
-
-
-
-class Usuario(models.Model):
-    Votos_std = models.EmbeddedField(
-        model_container=Votos_std,
-        model_form_class=Votos_stdForm
+    players = models.ArrayField(
+        model_container=Player,
+        model_form_class=PlayerForm
     )
-    Votos_taiko = models.EmbeddedField(
-        model_container=Votos_taiko,
-        model_form_class=Votos_taikoForm
-    )
-    Votos_ctb = models.EmbeddedField(
-        model_container=Votos_ctb,
-        model_form_class=Votos_ctbForm
-    )
-    Votos_mania = models.EmbeddedField(
-        model_container=Votos_mania,
-        model_form_class=Votos_maniaForm
-    )
-    
-    nickname = models.CharField(max_length=20)
-    id_osu_usuario = models.IntegerField()
-    puede_votar = forms.BooleanField()
-    
-    def __str__(self):
-        return self.nickname   
-   # objects = models.DjongoManager()
+
+    objects = models.DjongoManager()
