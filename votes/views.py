e99270521ad7a1b4d2a8e71d19376bdf0acbf019
Brayan <brayansachez67@hotmail.com>
2022-04-19 18:13:31 -0500

    settings things up to start with the Views and templates

--- votes/views.py
+++ votes/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Player, PlayerVotes
 
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>pekoepa</h1> mis panas")
 
 def vote(request):
-    return HttpResponse("<h1>prueba</h1>")
+    numero = Player.objects.all().count()
+    context = {
+        'numero': numero,
+    }
+    return render(request,'index.html', context=context)
